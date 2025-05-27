0bf6fca3226468022bb8cd7a0c14795785f6645d
matheusgmansano <m17jogos@gmail.com>
2025-05-27 00:50:29 -0300

    Add files via upload

---
+++ src/views/fase2.vue
@@ -0,0 +1,40 @@
+<script>
+import { mapa2 } from '@/mapas/mapa2.js';
+import createScript from '@/scripts/jogo';
+import '@/styles/elementosMapa.css';
+import '@/styles/body.css';
+import '@/styles/botoes.css';
+import '@/styles/telaVitoria.css';
+
+export default createScript(mapa2, '/fase3');
+</script>
+
+<template>
+
+  <title>Fase 2</title>
+  <button @click="voltar" class="botaoVoltar">INÍCIO</button>
+
+  <div class="tela">
+    <h1>Fase 2</h1>
+    <div class="mapa">
+      <div v-for="(linha, y) in mapa" :key="y" class="linha">
+        <div v-for="(celula, x) in linha" :key="x" :class="getClasse(y, x)">
+
+        </div>
+      </div>
+    </div>
+    <button @click="reiniciar" class="botaoReiniciar">Reiniciar</button>
+  </div>
+
+  <div v-if="mostrarProximaFase" class="telaVitoria">
+    <div class="janela">
+      <h2>Fase Concluída!</h2>
+      <button @click="irParaProximaFase" class="botaoVitoria">Próxima Fase</button>
+      <button @click="reiniciar" class="botaoVitoria">Reiniciar</button>
+      <button @click="voltar" class="botaoVitoria">Menu</button>
+    </div>
+  </div>
+
+</template>
+
+<style></style>
---
+++ src/views/telaInicial.vue
@@ -0,0 +1,49 @@
+<script scoped>
+import '@/styles/botoes.css';
+import '@/styles/body.css';
+
+
+export default {
+  methods: {
+    irParaLabirinto() {
+      this.$router.push('/fase1')
+    }
+  }
+}
+</script>
+
+<template>
+  <title>Tela Inicial</title>
+
+  <div class="tela">
+    <h1 class="titulo">Push the Box</h1>
+    <button @click="irParaLabirinto" class="botaoIniciar">INICIAR</button>
+  </div>
+
+</template>
+
+<style scoped>
+.titulo {
+  color: rgb(255, 255, 255);
+  font-family: 'Silkscreen', monospace;
+  font-size: 60px;
+  animation: pulse 8s infinite;
+}
+
+@keyframes pulse {
+  0% {
+    transform: scale(1);
+    opacity: 1;
+  }
+
+  50% {
+    transform: scale(1.10);
+    opacity: 0.8;
+  }
+
+  100% {
+    transform: scale(1);
+    opacity: 1;
+  }
+}
+</style>
