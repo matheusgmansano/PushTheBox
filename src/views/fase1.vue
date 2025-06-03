82cf77f87d9dec8ca0fbfc37e76cf3190e6ae50e
matheusgmansano <matheusmansano17@gmail.com>
2025-06-03 02:18:02 -0300

    t

---
+++ src/views/fase1.vue
@@ -0,0 +1,40 @@
+<script>
+import { mapa1 } from '@/mapas/mapa1.js';
+import createScript from '@/scripts/jogo';
+import '@/styles/elementosMapa.css';
+import '@/styles/body.css';
+import '@/styles/botoes.css';
+import '@/styles/telaVitoria.css';
+
+export default createScript(mapa1, '/fase2');
+</script>
+
+<template>
+
+  <title>Fase 1</title>
+  <button @click="voltar" class="botaoVoltar">INÍCIO</button>
+
+  <div class="tela">
+    <h1>Fase 1</h1>
+    <div class="mapa">
+      <div v-for="(linha, y) in mapa" :key="y" class="linha">
+        <div v-for="(celula, x) in linha" :key="x" :class="getClasse(y, x)">
+
+        </div>
+      </div>
+    </div>
+    <button @click="reiniciar" class="botaoReiniciar">Reiniciar</button>
+  </div>
+
+  <div v-if="mostrarProximaFase" class="telaVitoria">
+    <div class="janela">
+      <h2>Fase Concluída!</h2>
+      <button @click="irParaProximaFase" class="botaoVitoria">Próxima Fase</button>
+      <button @click="reiniciar" class="botaoVitoria">Reiniciar</button>
+      <button @click="voltar" class="botaoVitoria">Menu</button>
+    </div>
+  </div>
+
+</template>
+
+<style></style>
